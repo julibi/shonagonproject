ca84fe0b2aeaf80ce562749050ec23d62ff61d7b
julibi <hyunkyung.yi@yahoo.de>
2017-09-15 01:10:24 +0200

    first draft of version-b done

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,26 @@
 var getButtonId = [];
+var newSnu = [];
+
 nextSnusButtons.forEach(function(nextSnuButton){
 	getButtonId.push(nextSnuButton.id);
 	console.log(getButtonId);
 });
 getButtonId.forEach(function(element){
-	var element = document.getElementById(element);
-	console.log(element);
-	element.addEventListener('click', function(){
-		console.log('starting next step...');
-		console.log(snus.element);
+	var elementButton = document.getElementById(element);
+	elementButton.addEventListener('click', function(){
+		function newOne(snu){
+			if (snu.id === element) {
+				return snu
+			};
+		}
+		newSnu = snus.filter(newOne);
+		console.log(newSnu);
+		title.innerHTML = newSnu[0].title;
+		title.id = newSnu[0].id;
+		output.innerHTML = newSnu[0].text;
+		while(btnContainer.firstChild) {
+			btnContainer.removeChild(btnContainer.firstChild);
+		}
 	});
 });
 
