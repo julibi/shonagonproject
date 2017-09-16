a1442d345a5f09489c17345fa94b26d1056556e3
julibi <hyunkyung.yi@yahoo.de>
2017-09-16 16:55:46 +0200

    stuck when trying to obtain an object - filterin the array of snus with an id shouldn't be problem should it??

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,64 @@
-var getButtonId = [];
-var newSnu = [];
+var btnContainer = document.getElementById('button-container');
+var title = document.getElementById('title');
+var output = document.getElementById('output');
+var container = document.getElementById('container-fluid');
 
-nextSnusButtons.forEach(function(nextSnuButton){
-	getButtonId.push(nextSnuButton.id);
-	console.log(getButtonId);
-});
-getButtonId.forEach(function(element){
-	var elementButton = document.getElementById(element);
-	elementButton.addEventListener('click', function(){
-		function newOne(snu){
-			if (snu.id === element) {
-				return snu
-			};
-		}
-		newSnu = snus.filter(newOne);
-		console.log(newSnu);
-		title.innerHTML = newSnu[0].title;
-		title.id = newSnu[0].id;
-		output.innerHTML = newSnu[0].text;
-		while(btnContainer.firstChild) {
-			btnContainer.removeChild(btnContainer.firstChild);
-		}
-	});
-});
+function createHTML(selectedObj){
+	title.innerHTML = selectedObj.title;
+	output.innerHTML = selectedObj.text;
+}
+
+function selectRandomKeyword(obj) {
+  const max = obj['keywords'].length;
+  const random = Math.floor(Math.random() * max);
+  return obj['keywords'][random];
+}
+
+function findObjsWithKeyword(snus, keyword) {
+  const foundArray = [];
+  snus.forEach(obj => {
+    if (obj['keywords'].includes(keyword)) {
+      foundArray.push(obj);
+    }
+  });
+  return foundArray;
+}
+
+//the Object shown in the beginning
+//I hope to be able to reassign selectedObj each time when a button is clicked
+let selectedObj = snus[13];
+
+createHTML(selectedObj);
+
+const keyword = selectRandomKeyword(selectedObj);
+
+const newObjs = findObjsWithKeyword(snus, keyword);
+
+//we don't want to see objects we have already read again
+var index = newObjs.indexOf(selectedObj);
+newObjs.splice(index, 1);
+
+newObjs;
+
+//create Button
+for(var i = 0; i < newObjs.length; i++){
+	var btn = document.createElement('button');
+	btn.id = newObjs[i].id;
+	btn.innerHTML = newObjs[i].title;
+	btn.addEventListener('click', onButtonClick());
+	btnContainer.append(btn);
+}
+
+function onButtonClick() {
+	var searchId = this.id;
+  	const match = [];
+  	console.log(newObjs, searchId);
+  	newObjs.forEach(obj => {
+    	if (obj['id'] === searchId) {
+      		match.push(obj);
+    	}
+  	});
+  	return match;
+}
+
 
